a9119f4fcb9726128e5aeab82a941fc1d7b5472c
meowphetamine <meowphetamine@gmail.com>
2025-06-30 02:58:05 -0700

    Added react-router for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import DetailPackages from "./pages/DetailPackages";
+import PaintCorrection from "./pages/PaintCorrection";
+import CeramicCoating from "./pages/CeramicCoating";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-export default function LandingPage() {
+export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
-        <p className="text-black">Still under construction lol.</p>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/detail-packages" element={<DetailPackages />} />
+          <Route path="/paint-correction" element={<PaintCorrection />} />
+          <Route path="/ceramic-coating" element={<CeramicCoating />} />
+        </Routes>
       </main>
       <Footer />
     </div>
