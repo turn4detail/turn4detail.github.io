337494ae563ce2bc0211cc57ceedef79eba118ed
meowphetamine <meowphetamine@gmail.com>
2025-07-01 23:26:28 -0700

    404 not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./pages/Home";
+import NotFound404 from "./pages/NotFound404";
 import About from "./pages/About";
 import DetailPackages from "./pages/DetailPackages";
 import PaintCorrection from "./pages/PaintCorrection";
@@ -14,6 +15,7 @@ export default function App() {
       <main className="flex-grow">
         <Routes>
             <Route path="/" element={<Home />} />
+             <Route path="/404-not-found" element={<NotFound404 />} />
             <Route path="/about" element={<About />} />
             <Route path="/detail-packages" element={<DetailPackages />} />
             <Route path="/paint-correction" element={<PaintCorrection />} />
---
+++ src/pages/NotFound404.tsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero';
+import Section from '../components/Section';
+
+export default function NotFound404() {
+  return (
+    <div>
+      <Hero
+        bg="/background/404-not-found-bg-1.jpg"
+        subheading="This page is still under construction!"
+        heading=""
+        highlight="404 NOT FOUND"
+      />
+    </div>
+  );
+}
